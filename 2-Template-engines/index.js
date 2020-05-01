3252fa9b83e0e96a445b5a69ec82c637e429f782
Quan <quan.nh148983@sis.hust.edu.vn>
2020-05-01 21:48:01 +0700

    Bai 3

--- 2-Template-engines/index.js
+++ 2-Template-engines/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 var app = express();
 var port = 3000;
 
+
+var products = [
+	{id:1, name:'ahihi'},
+	{id:2, name:'24234234235'}
+];
 // PUG template
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -10,13 +15,18 @@ app.get('/', function(req, res){
 	res.render('index');
 });
 app.get('/cart', function(req, res){
-	res.render('cart/cartlist', {
-		products: [
-			{id:1, name:'adadad'},
-			{id:2, name:'adadad'}
-		]
+	res.render('cart/cartlist', { products: products
 	});
 });
+app.get('/cart/search', function(req, res){
+	var q =  req.query.q;
+	var filted = products.filter(function(item){
+		return item.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('cart/cartlist', { products: filted });
+});
+// Bài 3 
+
 
 app.listen(port, function(){
 	console.log('Server Start in port: '+ port);
